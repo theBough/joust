620a1b4f6d297973ac094d222e0f028093d52cf0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-04 09:24:24 -0500

    Blank template to get started

---
+++ ostrich.js
@@ -0,0 +1,39 @@
+function Ostrich(x,y,w,h){
+  //declare your variables here
+  
+  this.display = function(){
+   
+  
+    
+  }
+  
+  this.update = function(){
+   //this if-block will be our gravity.
+    if(this.y<  260){
+      this.y += 4
+      this.isJumping = true;
+    }else{
+      this.isJumping = false;
+    }
+    
+    if(keyIsDown(38)){
+      this.y -= this.jumpStrength;
+    }//end if
+    if(keyIsDown(37)){
+     
+     this.animateImagesLt() 
+    }//end if
+    if(keyIsDown(39)){
+      
+     this.animateImagesRt()
+    }//end if
+    
+  }//end update
+  this.animateImagesRt = function(){
+   
+    }//end Switch
+  }
+  this.animateImagesLt = function(){
+   
+  }
+}//end Ostrich
