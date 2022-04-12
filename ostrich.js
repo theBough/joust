8ade19b3f21cd8f2735c9cae10a3e452bf185d8e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-12 11:30:48 -0500

    Should animate RIGHT

--- ostrich.js
+++ ostrich.js
@@ -1,12 +1,18 @@
 function Ostrich(x,y,w,h){
   //declare your variables here
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.img = loadImage("images/rtOne.png")
   this.isJumping = false;
   this.jumpStrength = 10;
+  this.speed = 5;
+  this.imageCycleRt = 0;
   
   this.display = function(){
-   
-  
-    
+   this.img.resize(this.w, this.h);
+    image(this.img, this.x, this.y)  
   }
   
   this.update = function(){
@@ -22,18 +28,42 @@ function Ostrich(x,y,w,h){
       this.y -= this.jumpStrength;
     }//end if
     if(keyIsDown(37)){
-     
+     this.x -= this.speed
      this.animateImagesLt() 
     }//end if
     if(keyIsDown(39)){
-      
+      this.x += this.speed
+     
      this.animateImagesRt()
     }//end if
     
   }//end update
   this.animateImagesRt = function(){
+    /*every 5 interations of this functions
+    we will change the image of the ostrich to give
+    the illusion of an animation*/
    
-    }//end Switch
+    this.imageCycleRt +=1;
+    console.log(this.imageCycleRt)
+    switch(this.imageCycleRt){
+         
+      case 1:
+        this.img = loadImage("images/rtOne.png");
+        //console.log("Hello")
+        break;
+      case 5:
+        this.img = loadImage("images/rtTwo.png");
+        break;
+      case 10:
+        this.img = loadImage("images/rtThree.png");
+        break;
+      case 15:
+        this.img = loadImage("images/rtFour.png");
+        this.imageCycleRt = -3
+        break;
+    }//end switch
+   
+    
   }
   this.animateImagesLt = function(){
    
